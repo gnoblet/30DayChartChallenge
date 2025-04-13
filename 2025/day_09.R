7172fd06bb325d4a468d7821322fc5cdf150e4b5
gnoblet <gnoblet@zaclys.net>
2025-04-13 11:45:29 +0200

    add day09

--- 2025/day_09.R
+++ 2025/day_09.R
@@ -2,7 +2,6 @@ library(owidapi)
 library(ggplot2)
 library(data.table)
 library(geofacet)
-library(ggtext)
 library(showtext)
 
 
@@ -27,26 +26,15 @@ dat <-  dat[, entity_name := fcase(
 # merge and remove empty codes
 dat_cty <- dat[cty, on = .(entity_name = name)][!is.na(code)]
 dat_cty[, avg_score := mean(cantril_ladder_score, na.rm = TRUE), by = year]
-dat_cty[,
-  score_category := fifelse(
-    cantril_ladder_score > avg_score,
-    "Above Average",
-    "Below Average")
-  ]
-
 
 #------ Colors and fonts
 font_add_google("Nunito", "Nunito")
 showtext_auto()
 
-body_col <- "#8B4513"
-bg_col <- "#F5F5DC"
 body_font <- "Nunito"
 title_font <- "Nunito"
-
-# Plot stacked area chart# Modern color scheme
-modern_fill <- "#00CFC8"
-modern_line <- "#112D4E"
+col_fill <- "#00CFC8"
+col_line <- "#112D4E"
 panel_bg_col <- "#F0F0F0"
 bg_col <- "#FFFFFF"
 text_col <- "#2A2A2A"
@@ -60,14 +48,14 @@ tag <- "
 
   <span style='font-size:12pt;color:#2A2A2A;'>This visualization focuses on African countries from 2011 to 2020. The <span style='color:#00CFC8;'>**aquamarine areas**</span> show yearly country averages, while the <span style='color:#112D4E;'>**midnight line**</span> represents the average score across all African countries This average provides a benchmark for comparison.</span>
 
-  <span style='font-size:12pt;color:#2A2A2A;'> It is worth highlighting there is missing data for given years, for instance in Djibouti (DJ) or South Sudan (SSD) or entirely for certain countries like Eritrea (ER) or Capo Verde (CV)<br>.
+  <span style='font-size:12pt;color:#2A2A2A;'> Note data is missing for given years, for instance in Djibouti (DJ) or South Sudan (SSD), or entirely for certain countries like Eritrea (ER) or Capo Verde (CV)<br>.
 
-  <span style='font-size:9pt;color:#6D6D6D;'>Data: Our World in Data on Self-Reported Life Satisfaction | Visualization: @gnoblet</span>
+  <span style='font-size:9pt;color:#6D6D6D;'>Data: Our World in Data on Self-Reported Life Satisfaction | Viz: @gnoblet</span>
 "
-# Modernized plot
+
 g <- ggplot(dat_cty, aes(x = year, y = cantril_ladder_score, group = code)) +
-  geom_area(fill = modern_fill, alpha = 0.3) +
-  geom_line(aes(y = avg_score), color = modern_line, linewidth = 0.5) +
+  geom_area(fill = col_fill, alpha = 0.3) +
+  geom_line(aes(y = avg_score), color = col_line, linewidth = 0.5) +
   labs(
     tag = tag,
     x = NULL,
@@ -82,24 +70,23 @@ g <- ggplot(dat_cty, aes(x = year, y = cantril_ladder_score, group = code)) +
     panel.background = element_rect(fill = panel_bg_col, color = NA),
     plot.background = element_rect(fill = bg_col, color = NA),
     panel.grid = element_blank(),
-    plot.margin = margin(20,280, 20, 20),
+    plot.margin = margin(20,450, 20, 20),
     plot.tag.position = c(1.1, 0.5),
     plot.tag = element_textbox_simple(
-      width = unit(3.5, "inch"),
+      width = unit(4.3, "inch"),
       lineheight = 1.2,
       hjust = 0,
-      maxwidth = 0.8
+      maxwidth = 1
     ),
     strip.text = element_text(color = text_col, size = 9)
   )
+
 g
 
-# ggsave
 # save
 ggsave(
   "2025/day_09.png",
   dpi = 600,
-  width = 8,
-  height = 10,
-  type = "cairo-png"
+  width = 11,
+  height = 11,
 )
